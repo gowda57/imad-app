ea0dc90043d624c638cbf6c93010008fc32bd585
GOWTHAM GOWDA T C <gowthamgowda57@gmail.com>
2017-09-04 22:43:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,8 +90,8 @@ res.send(createTemplate(articles[articleName]));
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
 
 
