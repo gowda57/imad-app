2eed9d8b2a94d4e27eff3b89477922001f1c1f13
GOWTHAM GOWDA T C <gowthamgowda57@gmail.com>
2017-09-07 09:10:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@
 var button = document.getElementById('counter');
 
 button.onclick = function () {
-    var request = new XMLHttpResponse();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-     if(request.readystate === XMLHttpResponse.DONE) {
+     if(request.readystate === XMLHttpRequest.DONE) {
          if(request.status === 200) {
              var counter = request.responseText;
              var span = document.getElementById('count');
