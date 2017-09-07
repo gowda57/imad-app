7120dfaaf09a8e3279c537876a3f8d9669993d4a
GOWTHAM GOWDA T C <gowthamgowda57@gmail.com>
2017-09-07 09:05:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,18 @@
 // Counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+    var request = new XMLHttpResponse();
+    request.onreadystatechange = function () {
+     if(request.readystate === XMLHttpRequest.DONE) {
+         if(request.status === 200) {
+             var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString(); 
+         }
+     }
+      
+    };
+ request.open('GET', 'http://gowthamgowda57.imad.hasura-app.io/counter', true);
+ request.send(null);
 };
